37cc4128d9b76b5ab593e9417e4f26deb6f8fdd0
Alexander Overvoorde <overv161@gmail.com>
2015-04-22 10:57:08 +0200

    Add webcam test scene

---
+++ New Unity Project/Assets/Webcam.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Webcam : MonoBehaviour {
+    //public MeshRenderer[] UseWebcamTexture;
+    private WebCamTexture webcamTexture;
+
+    public string webcamName;
+
+    void Start() {
+        foreach (WebCamDevice dev in WebCamTexture.devices) {
+            //Debug.Log(dev.name);
+        }
+
+        webcamTexture = new WebCamTexture(webcamName, 320, 240);
+        /*foreach (MeshRenderer r in UseWebcamTexture) {
+            r.material.mainTexture = webcamTexture;
+        }*/
+        //renderer.material.mainTexture = webcamTexture;
+        gameObject.GetComponent<Renderer>().material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+
+        Debug.Log(webcamTexture.width + ", " + webcamTexture.height);
+    }
+
+    void OnGUI() {
+        if (webcamTexture.isPlaying) {
+            if (GUILayout.Button("Pause")) {
+                webcamTexture.Pause();
+            }
+            if (GUILayout.Button("Stop")) {
+                webcamTexture.Stop();
+            }
+        } else {
+            if (GUILayout.Button("Play")) {
+                webcamTexture.Play();
+            }
+        }
+    }
+}
