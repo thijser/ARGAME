a98410f2c812a2e18c3942d5a0ef034059b0bd41
Patrick Kramer <ptrck.krmr@gmail.com>
2015-04-29 19:33:49 +0200

    Formatted NetworkManager and fixed some style issues
    Added missing brackets and removed empty methods.

--- New Unity Project/Assets/Scripts/NetworkManager.cs
+++ New Unity Project/Assets/Scripts/NetworkManager.cs
@@ -22,53 +22,48 @@ public class NetworkManager : MonoBehaviour
   void OnGUI()
   {
     if (!Network.isClient && !Network.isServer)
+    {
+      if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
       {
-        if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
-          StartServer();
-
-        if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
-          RefreshHostList();
-
-        if (hostList != null)
-          {
-            for (int i = 0; i < hostList.Length; i++)
-              {
-                if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList [i].gameName))
-                  JoinServer(hostList [i]);
-              }
-          }
+        StartServer();
       }
+      if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+      {
+        RefreshHostList();
+      }
+      if (hostList != null)
+      {
+        for (int i = 0; i < hostList.Length; i++)
+        {
+          if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList [i].gameName))
+          {
+            JoinServer(hostList [i]);
+          }
+        }
+      }
+    }
   }
-        // Use this for initialization
-        void Start()
-        {
 
-        }
+  private void RefreshHostList()
+  {
+    MasterServer.RequestHostList(typeName);
+  }
 
-        // Update is called once per frame
-        void Update()
-        {
+  void OnMasterServerEvent(MasterServerEvent msEvent)
+  {
+    if (msEvent == MasterServerEvent.HostListReceived)
+    {
+      hostList = MasterServer.PollHostList();
+    }
+  }
 
-        }
+  private void JoinServer(HostData hostData)
+  {
+    Network.Connect(hostData);
+  }
 
-        private void RefreshHostList()
-        {
-          MasterServer.RequestHostList(typeName);
-        }
-
-        void OnMasterServerEvent(MasterServerEvent msEvent)
-        {
-          if (msEvent == MasterServerEvent.HostListReceived)
-            hostList = MasterServer.PollHostList();
-        }
-
-        private void JoinServer(HostData hostData)
-        {
-          Network.Connect(hostData);
-        }
-
-        void OnConnectedToServer()
-        {
-          Debug.Log("Server Joined");
-        }
+  void OnConnectedToServer()
+  {
+    Debug.Log("Server Joined");
+  }
 }
