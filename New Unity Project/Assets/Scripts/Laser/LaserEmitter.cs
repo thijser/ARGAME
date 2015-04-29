cb53ce121f44642d635becdc12d65e440f52f321
Patrick Kramer <ptrck.krmr@gmail.com>
2015-04-29 13:11:46 +0200

    Removed debug statements.

--- New Unity Project/Assets/Scripts/Laser/LaserEmitter.cs
+++ New Unity Project/Assets/Scripts/Laser/LaserEmitter.cs
@@ -13,14 +13,12 @@ namespace Laser {
 		private List<Laser> segments = new List<Laser>();
 
 		public void Update() {
-			print ("Emitter.Update");
 			Clear ();
 			MakeLaser ();
 			Render ();
 		}
 
 		public void MakeLaser() {
-			print ("Emitter.MakeLaser");
 			Vector3 pos = gameObject.transform.position + Offset;
 			Quaternion dir = gameObject.transform.rotation;
 			
@@ -33,15 +31,10 @@ namespace Laser {
 		}
 
 		public void Render() {
-			print ("   " + segments [0].origin);
-			foreach (var l in segments) {
-				print ("   " + l.endpoint);
-			}
 			lineRenderer.SetVertexCount (segments.Count + 1);
 			Vector3 renderOrigin = segments[0].origin + gameObject.transform.forward * RenderOffset;
 			lineRenderer.SetPosition (0, renderOrigin);
 			for (int i=0; i<segments.Count; i++) {
-				print ("  Drawn segment #" + i + " to " + segments[i].endpoint);
 				lineRenderer.SetPosition(i+1, segments[i].endpoint);
 			}
 		}
