fc97d99fd522167cd4b29e1d8ac876e570254393
Tim van Rossum <trvanrossum@gmail.com>
2015-04-30 11:19:01 +0200

    Started development on randomized levels.

---
+++ New Unity Project/Assets/Scripts/RandomLevel/SquareGraph.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace RandomLevel{
+	public class SquareGraph : MonoBehaviour {
+		
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
