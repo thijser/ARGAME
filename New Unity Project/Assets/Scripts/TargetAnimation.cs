6af6243e2ea1254a604982bad1663f84926e96e5
thijs boumans <thijsboumans@online.nl>
2015-04-30 11:58:45 +0200

    work on animations

---
+++ New Unity Project/Assets/Scripts/TargetAnimation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetAnimation : MonoBehaviour {
+	Animation anim;
+	public AnimationClip Open; 
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation>();
+		anim.AddClip (Open, "Open");
+		anim.Play ("Open");
+
+	}
+}
