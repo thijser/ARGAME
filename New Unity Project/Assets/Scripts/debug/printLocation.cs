d09cd6c6a7f0a52ae446b8f1442b6c4f0e734e42
thijs boumans <thijsboumans@online.nl>
2015-05-06 10:19:30 +0200

    checkouttheis

---
+++ New Unity Project/Assets/Scripts/debug/printLocation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class printLocation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (" hello");
+		Debug.Log ("position:"  + transform.position);
+		Debug.Log ("rotation:" + transform.rotation);
+	}
+}
