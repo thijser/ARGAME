ec99077e99497a2980abc8a0dd05294ac535230f
Tim van Rossum <trvanrossum@gmail.com>
2015-05-06 09:50:12 +0200

    RandomLevelGenerator now throws an exception if rows or cols is less than 10.

--- New Unity Project/Assets/Scripts/RandomLevel/RandomLevelGenerator.cs
+++ New Unity Project/Assets/Scripts/RandomLevel/RandomLevelGenerator.cs
@@ -21,6 +21,10 @@ namespace RandomLevel{
 		///<param name="rows">The amount of rows</param>
 		///<param name="cols">The amount of columns</param>
 		public RandomLevelGenerator(int rows, int cols){
+			if (rows < 10 && cols < 10) {
+				throw new ArgumentException("The amount of rows and the amount of cols" +
+					"should both be at least 10.");
+			}
 			sg = new SquareGraph (rows, cols);
 			r = new Random (Environment.TickCount);
 			run();
