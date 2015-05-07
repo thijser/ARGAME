840b1ac7e865aece05ff8f75d8a7a0a5fcfcdc2e
thijs boumans <thijsboumans@online.nl>
2015-05-07 13:22:55 +0200

    added metaone

---
+++ New Unity Project/Assets/Scripts/projectionCode/ImageLinker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageLinker : MonoBehaviour {
+	public projectioncode Projector; 
+	public Transform linkedTo; 
+	void Start(){
+
+		Projector.addPosition (transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Projector.projectTransform(linkedTo,transform);
+		Projector.rotate (linkedTo);
+	}
+}
