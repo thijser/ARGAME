e7844762b2a81d45e13775dcb64b560b0244f7fe
thijs boumans <thijsboumans@online.nl>
2015-05-08 13:33:14 +0200

    position linkers have been improved

---
+++ New Unity Project/Assets/PositionLinker.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionLinker : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform LinkedTo;
+	
+	// Update is called once per frame
+	void Update () {
+		LinkedTo.position = transform.position;
+		LinkedTo.rotation = transform.rotation;
+	}
+}
