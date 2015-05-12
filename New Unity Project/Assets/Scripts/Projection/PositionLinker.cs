8d4af8c5eae6433975d3023caf4bb0f5a180785e
Patrick Kramer <ptrck.krmr@gmail.com>
2015-05-12 16:12:56 +0200

    Fixed namespace collisions. Fixes issue #6.

--- New Unity Project/Assets/Scripts/Projection/PositionLinker.cs
+++ New Unity Project/Assets/Scripts/Projection/PositionLinker.cs
@@ -1,108 +1,111 @@
 ﻿//----------------------------------------------------------------------------
 // <copyright file="PositionLinker.cs" company="Delft University of Technology">
 //     Copyright 2015, Delft University of Technology
-//     
+//
 //     This software is licensed under the terms of the MIT License.
-//     A copy of the license should be included with this software. If not, 
+//     A copy of the license should be included with this software. If not,
 //     see http://opensource.org/licenses/MIT for the full license.
 // </copyright>
 //----------------------------------------------------------------------------
-using System;
-using System.Collections;
-using System.Diagnostics.CodeAnalysis;
-using UnityEngine;
-
-/// <summary>
-/// Describes modes for linking position and rotation.
-/// </summary>
-public enum LinkingMode
+namespace Projection
 {
-    /// <summary>
-    /// Follow the exact position and rotation.
-    /// </summary>
-    Exact = 0,
+    using System;
+    using System.Collections;
+    using System.Diagnostics.CodeAnalysis;
+    using UnityEngine;
 
     /// <summary>
-    /// Ignore the y-rotation.
+    /// Describes modes for linking position and rotation.
     /// </summary>
-    IgnoreHeight = 1,
-
-    /// <summary>
-    /// Synchronize the position only, and ignore the rotation.
-    /// </summary>
-    PositionOnly = 2,
-
-    /// <summary>
-    /// Follow the rotation of the level marker.
-    /// </summary>
-    FollowLevel = 3,
-
-	/// <summary>
-	/// Project position to plane of level.
-	/// </summary>
-	Project = 4
-}
-
-/// <summary>
-/// Links to a Transform and keeps it updated as this game object's Transform changes.
-/// </summary>
-public class PositionLinker : MonoBehaviour
-{
-    /// <summary>
-    /// The Transform this PositionLinker is linked to.
-    /// </summary>
-    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Unity Property")]
-    public Transform LinkedTo;
-
-    /// <summary>
-    /// The LinkingMode to use.
-    /// </summary>
-    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Unity Property")]
-    public LinkingMode Mode = LinkingMode.FollowLevel;
-
-    /// <summary>
-    /// The Transform of the level marker.
-    /// </summary>
-    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Unity Property")]
-    public Transform LevelMarker;
-
-    /// <summary>
-    /// Updates the position and/or rotation of the LinkedTo Transform.
-    /// </summary>
-    public void Update()
+    public enum LinkingMode
     {
-		if (this.Mode == LinkingMode.Project) {
-			Vector3 v1 = transform.position - this.LevelMarker.position;
-			Vector3 n = this.LevelMarker.up;
-			
-			Vector3 proj = v1 - Vector3.Dot (v1, n) * n;
-			proj += this.LevelMarker.position;
+        /// <summary>
+        /// Follow the exact position and rotation.
+        /// </summary>
+        Exact = 0,
 
-			this.LinkedTo.position = proj;
-		} else {
-			this.LinkedTo.position = transform.position;
-		}
+        /// <summary>
+        /// Ignore the y-rotation.
+        /// </summary>
+        IgnoreHeight = 1,
 
-        switch (this.Mode)
+        /// <summary>
+        /// Synchronize the position only, and ignore the rotation.
+        /// </summary>
+        PositionOnly = 2,
+
+        /// <summary>
+        /// Follow the rotation of the level marker.
+        /// </summary>
+        FollowLevel = 3,
+
+    	/// <summary>
+    	/// Project position to plane of level.
+    	/// </summary>
+    	Project = 4
+    }
+
+    /// <summary>
+    /// Links to a Transform and keeps it updated as this game object's Transform changes.
+    /// </summary>
+    public class PositionLinker : MonoBehaviour
+    {
+        /// <summary>
+        /// The Transform this PositionLinker is linked to.
+        /// </summary>
+        [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Unity Property")]
+        public Transform LinkedTo;
+
+        /// <summary>
+        /// The LinkingMode to use.
+        /// </summary>
+        [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Unity Property")]
+        public LinkingMode Mode = LinkingMode.FollowLevel;
+
+        /// <summary>
+        /// The Transform of the level marker.
+        /// </summary>
+        [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Unity Property")]
+        public Transform LevelMarker;
+
+        /// <summary>
+        /// Updates the position and/or rotation of the LinkedTo Transform.
+        /// </summary>
+        public void Update()
         {
-            case LinkingMode.Exact:
-			case LinkingMode.Project:
-                this.LinkedTo.rotation = transform.rotation;
-                break;
-            case LinkingMode.IgnoreHeight:
-                Vector3 angles = transform.rotation.eulerAngles;
-                angles.x = this.LinkedTo.eulerAngles.x;
-				angles.y = this.LinkedTo.eulerAngles.y;
-                angles.z = this.LinkedTo.eulerAngles.z;
-                this.LinkedTo.rotation = Quaternion.Euler(angles);
-                break;
-            case LinkingMode.PositionOnly:
-                break;
-            case LinkingMode.FollowLevel:
-                this.LinkedTo.rotation = this.LevelMarker.rotation;
-                break;
-            default:
-                throw new ArgumentException("Invalid LinkingMode");
+    		if (this.Mode == LinkingMode.Project) {
+    			Vector3 v1 = transform.position - this.LevelMarker.position;
+    			Vector3 n = this.LevelMarker.up;
+
+    			Vector3 proj = v1 - Vector3.Dot (v1, n) * n;
+    			proj += this.LevelMarker.position;
+
+    			this.LinkedTo.position = proj;
+    		} else {
+    			this.LinkedTo.position = transform.position;
+    		}
+
+            switch (this.Mode)
+            {
+                case LinkingMode.Exact:
+    			case LinkingMode.Project:
+                    this.LinkedTo.rotation = transform.rotation;
+                    break;
+                case LinkingMode.IgnoreHeight:
+                    Vector3 angles = transform.rotation.eulerAngles;
+                    angles.x = this.LinkedTo.eulerAngles.x;
+    				angles.y = this.LinkedTo.eulerAngles.y;
+                    angles.z = this.LinkedTo.eulerAngles.z;
+                    this.LinkedTo.rotation = Quaternion.Euler(angles);
+                    break;
+                case LinkingMode.PositionOnly:
+                    break;
+                case LinkingMode.FollowLevel:
+                    this.LinkedTo.rotation = this.LevelMarker.rotation;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid LinkingMode");
+            }
         }
     }
 }
--- New Unity Project/Assets/Scripts/TargetColorPicker.cs
+++ New Unity Project/Assets/Scripts/TargetColorPicker.cs
@@ -1,13 +1,16 @@
-﻿using UnityEngine;
-using System.Collections;
+namespace Mirrors
+{
+	﻿using UnityEngine;
+	using System.Collections;
 
-public class TargetColorPicker : MonoBehaviour {
+	public class TargetColorPicker : MonoBehaviour {
 
-	public Color color;
-	// Use this for initialization
-	void Update () {
-		Renderer ren = gameObject.GetComponent<Renderer>();
-		ren.material.SetColor("Color",color);
+		public Color color;
+		// Use this for initialization
+		public void Update () {
+			Renderer ren = gameObject.GetComponent<Renderer>();
+			ren.material.SetColor("Color",color);
+		}
+
 	}
-
 }
