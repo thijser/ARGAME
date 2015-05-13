a2b3478b7315b2c084f5690ff30c63aa1bf4d40a
Tim van Rossum <trvanrossum@gmail.com>
2015-05-13 10:19:59 +0200

    Made a demo scene for our first demo (showing off game objects)

---
+++ New Unity Project/Assets/Scripts/ArrowKeyMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowKeyMove : MonoBehaviour {
+
+	private float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InputMovement ();
+	}
+
+	void InputMovement()
+	{
+		if (Input.GetKey (KeyCode.UpArrow))
+			GetComponent<Transform> ().position = GetComponent<Transform> ().position + Vector3.forward * speed * Time.deltaTime;
+		
+		if (Input.GetKey(KeyCode.DownArrow))
+			GetComponent<Transform> ().position = GetComponent<Transform> ().position - Vector3.forward * speed * Time.deltaTime;
+		
+		if (Input.GetKey(KeyCode.RightArrow))
+			GetComponent<Transform> ().position = GetComponent<Transform> ().position + Vector3.right * speed * Time.deltaTime;
+		
+		if (Input.GetKey(KeyCode.LeftArrow))
+			GetComponent<Transform> ().position = GetComponent<Transform> ().position - Vector3.right * speed * Time.deltaTime;
+	}
+}
