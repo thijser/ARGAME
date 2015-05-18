58cf11e2421ead8f3489cb455645e3d2aee857f5
Alexander Overvoorde <overv161@gmail.com>
2015-05-18 14:53:23 +0200

    Add Meta 1/Vuforia transformation scene

---
+++ New Unity Project/Assets/Scripts/Network/CubeCams.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+
+public class CubeCams : MonoBehaviour {
+    public Camera camLeft;
+    public Camera camRight;
+
+    private float xSeparation = 0.0f;
+    private float yOffset = 0.0f;
+    private float zOffset = 0.0f;
+    private float fov = 35.0f;
+
+    private float lastSave = 0.0f;
+
+    void Start() {
+        camLeft.aspect = 16f / 9f;
+        camRight.aspect = 16f / 9f;
+    }
+
+    void Update() {
+        float acc = 1.0f;
+        if (Input.GetKey(KeyCode.LeftShift)) acc = 10.0f;
+
+        if (Input.GetKey(KeyCode.W)) {
+            xSeparation += 0.01f * acc;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            xSeparation -= 0.01f * acc;
+        }
+        if (Input.GetKey(KeyCode.E)) {
+            zOffset += 0.01f * acc;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            zOffset -= 0.01f * acc;
+        }
+        if (Input.GetKey(KeyCode.R)) {
+            fov += 0.1f * acc;
+        }
+        if (Input.GetKey(KeyCode.F)) {
+            fov -= 0.1f * acc;
+        }
+        if (Input.GetKey(KeyCode.T)) {
+            yOffset += 0.01f * acc;
+        }
+        if (Input.GetKey(KeyCode.G)) {
+            yOffset -= 0.01f * acc;
+        }
+
+        xSeparation = Mathf.Max(0, xSeparation);
+
+        camLeft.transform.position = new Vector3(-xSeparation, yOffset, zOffset);
+        camRight.transform.position = new Vector3(xSeparation, yOffset, zOffset);
+
+        camLeft.fieldOfView = fov;
+        camRight.fieldOfView = fov;
+
+        if (Time.time - lastSave > 1.0f) {
+            File.WriteAllText("C:/Users/Alexander/Desktop/code.txt", xSeparation + "|" + yOffset + "|" + zOffset + "|" + fov);
+            lastSave = Time.time;
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Network/CubeClient.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+
+public class CubeClient : MonoBehaviour {
+    Socket client = null;
+
+    void Start() {
+        IPHostEntry ipHostInfo = Dns.Resolve("127.0.0.1");
+        IPAddress ipAddress = ipHostInfo.AddressList[0];
+        IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+        client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        client.Connect(remoteEP);
+        client.Blocking = false;
+    }
+
+    void Update() {
+        try {
+            byte[] buffer = new byte[256];
+            int len = client.Receive(buffer);
+
+            string[] parts = System.Text.Encoding.ASCII.GetString(buffer, 0, len).Split('|');
+
+            float px = float.Parse(parts[0]);
+            float py = float.Parse(parts[1]);
+            float pz = float.Parse(parts[2]);
+
+            float rx = float.Parse(parts[3]);
+            float ry = float.Parse(parts[4]);
+            float rz = float.Parse(parts[5]);
+            float rw = float.Parse(parts[6]);
+
+            transform.position = new Vector3(px, py, pz);
+            transform.rotation = new Quaternion(rx, ry, rz, rw);
+        } catch (SocketException) {
+            // Do nothing
+        }
+    }
+}
