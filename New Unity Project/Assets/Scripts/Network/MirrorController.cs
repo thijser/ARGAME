676547275a8dbd346eb196d02057ff22cc43649c
Patrick Kramer <ptrck.krmr@gmail.com>
2015-05-18 11:18:57 +0200

    Changed MirrorRotation (renamed to MirrorController) to properly select and rotate Mirrors.
    The script only needs to be assigned to a global object (but local to the remote player).
    Just like the original (MirrorRotation) script, the Materials for highlighted and original
    Mirror material must be set manually.

---
+++ New Unity Project/Assets/Scripts/Network/MirrorController.cs
@@ -0,0 +1,181 @@
+//----------------------------------------------------------------------------
+// <copyright file="MirrorController.cs" company="Delft University of Technology">
+//     Copyright 2015, Delft University of Technology
+//     
+//     This software is licensed under the terms of the MIT License.
+//     A copy of the license should be included with this software. If not, 
+//     see http://opensource.org/licenses/MIT for the full license.
+// </copyright>
+//----------------------------------------------------------------------------
+namespace Network
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using Core.Receiver;
+    using UnityEngine;
+
+    /// <summary>
+    /// Provides selection and rotation functionality to Mirrors.
+    /// </summary>
+    public class MirrorController : MonoBehaviour
+    {
+        /// <summary>
+        /// The Highlighted Material of the Mirror.
+        /// </summary>
+        [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Unity Property")]
+        public Material Highlight;
+
+        /// <summary>
+        /// The original Material of the Mirror.
+        /// </summary>
+        [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:FieldsMustBePrivate", Justification = "Unity Property")]
+        public Material Original;
+
+        /// <summary>
+        /// The selected Mirror.
+        /// </summary>
+        private Mirror selected;
+
+        /// <summary>
+        /// Gets or sets the selected Mirror.
+        /// </summary>
+        public Mirror SelectedMirror
+        {
+            get
+            {
+                return this.selected;
+            }
+
+            set
+            {
+                if (this.selected != value)
+                {
+                    this.ResetHighlight(this.selected);
+                    this.selected = value;
+                    this.HighlightMirror(this.selected);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the top parent in the hierarchy.
+        /// </summary>
+        /// <param name="transform">The Transform for which to locate the top parent, not null.</param>
+        /// <returns>The top parent.</returns>
+        public static Transform GetHighestParent(Transform transform)
+        {
+            if (transform == null)
+            {
+                throw new ArgumentNullException("transform");
+            }
+
+            Transform current = transform;
+            while (current.parent != null)
+            {
+                current = current.parent;
+            }
+
+            return current;
+        }
+
+        /// <summary>
+        /// Initializes this MirrorController and disables state synchronization for this
+        /// object.
+        /// </summary>
+        public void Start()
+        {
+            NetworkView view = this.GetComponent<NetworkView>();
+            view.stateSynchronization = NetworkStateSynchronization.Off;
+        }
+
+        /// <summary>
+        /// Updates the selected Mirror if the 'M' key is pressed and updates the 
+        /// rotation of the selected Mirror.
+        /// </summary>
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                this.UpdateSelectedMirror();
+            }
+
+            this.Rotate();
+        }
+
+        /// <summary>
+        /// Changes the selected Mirror to the next Mirror in sequence.
+        /// </summary>
+        private void UpdateSelectedMirror()
+        {
+            Mirror[] mirrors = GameObject.FindObjectsOfType<Mirror>();
+            if (mirrors.Length == 0)
+            {
+                this.SelectedMirror = null;
+                return;
+            }
+
+            // We need the Mirror at 'index + 2' because:
+            //   'index' is the current mirror. 
+            //   'index + 1' is the other side of the same mirror.
+            //   'index + 2' if the first side of the next mirror.
+            int index = Array.IndexOf(mirrors, this.SelectedMirror);
+            if (index + 2 >= mirrors.Length)
+            {
+                this.SelectedMirror = mirrors[0];
+            }
+            else
+            {
+                this.SelectedMirror = mirrors[index + 2];
+            }
+        }
+
+        /// <summary>
+        /// Rotates the mirror corresponding with whether the 'A' (left) or 'D' (right) key 
+        /// is pressed.
+        /// </summary>
+        private void Rotate()
+        {
+            if (this.SelectedMirror != null)
+            {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    float t = Time.deltaTime * -90f;
+                    this.SelectedMirror.transform.parent.Rotate(0, t, 0);
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    float t = Time.deltaTime * 90f;
+                    this.SelectedMirror.transform.parent.Rotate(0, t, 0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Highlights the mirror.
+        /// </summary>
+        /// <param name="mirror">The Mirror to highlight.</param>
+        private void HighlightMirror(Mirror mirror)
+        {
+            if (mirror != null)
+            {
+                Transform frame = mirror.transform.parent.Find("MirrorBase/Cube_002");
+                MeshRenderer mesh = frame.GetComponent<MeshRenderer>();
+                mesh.material = this.Highlight;
+            }
+        }
+
+        /// <summary>
+        /// Resets the Highlight.
+        /// </summary>
+        /// <param name="mirror">The Mirror to reset.</param>
+        private void ResetHighlight(Mirror mirror)
+        {
+            if (mirror != null)
+            {
+                Transform frame = mirror.transform.parent.Find("MirrorBase/Cube_002");
+                MeshRenderer mesh = frame.GetComponent<MeshRenderer>();
+                mesh.material = this.Original;
+            }
+        }
+    }
+}
