bd90907674d8cbcb91107bcb99a5dcf2c5ebb4fd
thijs boumans <thijsboumans@online.nl>
2015-05-20 10:36:12 +0200

    function signature for combining socket and core added
    Conflicts:
    	New Unity Project/Assets/RemoteObjectSyncer.cs.meta
    	New Unity Project/Assets/Scripts/Network/ClientSocket.cs.meta
    	New Unity Project/Assets/Scripts/Projection/RemoteObjectSyncer.cs
    	New Unity Project/Assets/Scripts/Projection/RemoteObjectSyncer.cs.meta

---
+++ New Unity Project/Assets/Scripts/Projection/RemoteObjectSyncer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RemoteObjectSyncer : MonoBehaviour {
+	public Dictionary<int,GameObject> objectTable;
+	public void SyncLoc (PositionUpdate P){
+	
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
