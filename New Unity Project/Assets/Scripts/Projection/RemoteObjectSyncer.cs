ab1976fc940f38d0141b621e10c5d8f0965a1ecb
thijs boumans <thijsboumans@online.nl>
2015-05-20 11:26:52 +0200

    improved scale finder

--- New Unity Project/Assets/Scripts/Projection/RemoteObjectSyncer.cs
+++ New Unity Project/Assets/Scripts/Projection/RemoteObjectSyncer.cs
@@ -3,13 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class RemoteObjectSyncer : MonoBehaviour {
-	public Hashtable objectTable;
+	public  Dictionary<int,GameObject> objectTable;
+	public List<GameObject> RegisterObjectsOnStartup;
 	public void SyncLoc (PositionUpdate P){
-	
+		if(!objectTable.ContainsKey(P.ID)){
+			throw new KeyNotFoundException("ID of not yet registered object");
+		}
+		GameObject toMove=objectTable[P.ID];
+		Transform transToMove = toMove.GetComponent<Transform>();
+		transToMove.localPosition = new Vector3(P.X*transform.localScale.x,0,P.Y*transform.localScale.y);
 	}
-	// Use this for initialization
+	public void RegisterObject(int id,GameObject obj){
+		objectTable.Add(id,obj);
+	}
+	// load all objects in list into memory; 
 	void Start () {
-	
+		int i=0;
+		foreach(GameObject go in RegisterObjectsOnStartup){
+			i++;
+			objectTable.Add(i,go);
+			Transform transGo = Transform(go);
+			transGo.SetParent(this);
+		}
 	}
 	
 	// Update is called once per frame
