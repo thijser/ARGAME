4a0e87d6ea79ec275b343e50de6bd1ea5b36d75b
thijs boumans <thijsboumans@online.nl>
2015-05-26 12:20:07 +0200

    migrated some scripts

---
+++ ARGame/scripts/Core/Receiver/BeamSplitter.cs
@@ -0,0 +1,67 @@
+﻿//----------------------------------------------------------------------------
+// <copyright file="BeamSplitter.cs" company="Delft University of Technology">
+//     Copyright 2015, Delft University of Technology
+//     
+//     This software is licensed under the terms of the MIT License.
+//     A copy of the license should be included with this software. If not, 
+//     see http://opensource.org/licenses/MIT for the full license.
+// </copyright>
+//----------------------------------------------------------------------------
+namespace Core.Receiver
+{
+    using System;
+    using System.Collections.Generic;
+    using Core.Emitter;
+    using UnityEngine;
+
+    /// <summary>
+    /// An object that splits the beam into a reflection and pass-through ray.
+    /// </summary>
+    public class BeamSplitter : MonoBehaviour, ILaserReceiver
+    {
+        /// <summary>
+        /// Gets or sets the LaserEmitter used for creating new Laser beam segments.
+        /// </summary>
+        public MultiEmitter PassThroughEmitter { get; set; }
+
+        /// <summary>
+        /// Initialize by creating emitter object.
+        /// </summary>
+        public void Start()
+        {
+            this.PassThroughEmitter = gameObject.AddComponent<MultiEmitter>();
+        }
+
+        /// <summary>
+        /// Reflects the argument Laser beam and emits a new Laser beam that
+        /// passes through.
+        /// </summary>
+        /// <param name="sender">The sender of the event, ignored here.</param>
+        /// <param name="args">The EventArgs object that describes the event.</param>
+        public void OnLaserHit(object sender, HitEventArgs args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException("args");
+            }
+
+            if (!args.IsValid)
+            {
+                throw new ArgumentException("The supplied HitEventArgs object is invalid.");
+            }
+
+            // Create a new ray coming out of the other side with the same direction
+            // as the original ray. Forward needs to be negative, see LaserEmitter.
+            var passThroughEmitter = this.PassThroughEmitter.GetEmitter(args.Laser);
+
+            passThroughEmitter.transform.position = args.Point + (args.Laser.Direction * 0.1f);
+            passThroughEmitter.transform.forward = -args.Laser.Direction;
+            LaserProperties propertiesPre = args.Laser.Emitter.GetComponent<LaserProperties>();
+            LaserProperties propertiesPost = passThroughEmitter.GetComponent<LaserProperties>();
+            propertiesPost.RGBStrengths = propertiesPre.RGBStrengths / 2;
+
+            // Create the second ray, reflecting off surface like a mirror
+            Mirror.CreateReflection(args.Laser, args.Normal);
+        }
+    }
+}
---
+++ ARGame/scripts/Network/PositionUpdate.cs
@@ -0,0 +1,111 @@
+﻿//----------------------------------------------------------------------------
+// <copyright file="PositionUpdate.cs" company="Delft University of Technology">
+//     Copyright 2015, Delft University of Technology
+//     
+//     This software is licensed under the terms of the MIT License.
+//     A copy of the license should be included with this software. If not, 
+//     see http://opensource.org/licenses/MIT for the full license.
+// </copyright>
+//----------------------------------------------------------------------------
+namespace Network
+{
+    using System;
+
+    /// <summary>
+    /// Represents an update of a marker position.
+    /// </summary>
+    public class PositionUpdate
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Network.PositionUpdate"/> class.
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="rotation">The rotation.</param> 
+        /// <param name="id">The unique ID of the marker.</param>
+        /// <param name="timestamp">The timestamp of the update.</param>
+        public PositionUpdate(float x, float y, float rotation, int id, long timestamp)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Rotation = rotation;
+            this.ID = id;
+            this.TimeStamp = timestamp;
+        }
+
+        /// <summary>
+        /// Gets the x coordinate of this update.
+        /// </summary>
+        public float X { get; private set; }
+
+        /// <summary>
+        /// Gets the y coordinate of this update.
+        /// </summary>
+        public float Y { get; private set; }
+
+        /// <summary>
+        /// Gets the rotation of this update.
+        /// </summary>
+        public float Rotation { get; private set; }
+
+        /// <summary>
+        /// Gets the unique ID of the marker object.
+        /// </summary>
+        public int ID { get; private set; }
+
+        /// <summary>
+        /// Gets the timestamp of this update.
+        /// </summary>
+        public long TimeStamp { get; private set; }
+
+        /// <summary>
+        ///   Serves as a hash function for a <see cref="Network.PositionUpdate"/> object.
+        /// </summary>
+        /// <returns>
+        ///   A hash code for this instance that is suitable for use in hashing 
+        ///   algorithms and data structures such as a hash table.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return (int)(Math.Pow((float)this.TimeStamp, (float)this.ID) * Math.Pow(this.X, this.Y));
+        }
+
+        /// <summary>
+        ///   Determines whether the specified <see cref="System.Object"/> is equal to the 
+        ///   current <see cref="Network.PositionUpdate"/>.
+        /// </summary>
+        /// <param name="o">
+        ///   The <see cref="System.Object"/> to compare with the current 
+        ///   <see cref="Network.PositionUpdate"/>.
+        /// </param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object"/> is equal to the current
+        ///   <see cref="Network.PositionUpdate"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object o)
+        {
+            if (o == null || o.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            PositionUpdate that = o as PositionUpdate;
+            return this.X == that.X
+                && this.Y == that.Y
+                && this.ID == that.ID
+                && this.TimeStamp == that.TimeStamp;
+        }
+
+        /// <summary>
+        ///   Returns a <see cref="System.String"/> that represents the current <see cref="Network.PositionUpdate"/>.
+        /// </summary>
+        /// <returns>
+        ///   A <see cref="System.String"/> that represents the current <see cref="Network.PositionUpdate"/>.
+        /// </returns>
+        public override string ToString()
+        {
+            return "[PositionUpdate: ID<" + this.ID + ">, X<" + this.X + 
+                   ">, Y<" + this.Y + ">, TimeStamp<" + this.TimeStamp + ">]";
+        }
+    }
+}
