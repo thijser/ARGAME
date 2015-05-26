f0e8be2f2842ac3298f860d12f69c4c19098c755
Patrick Kramer <ptrck.krmr@gmail.com>
2015-05-26 09:57:17 +0200

    Added missing documentation and fixed some spelling errors.

--- New Unity Project/Assets/Scripts/Projection/UsedCardManager.cs
+++ New Unity Project/Assets/Scripts/Projection/UsedCardManager.cs
@@ -1,9 +1,9 @@
 ﻿//----------------------------------------------------------------------------
 // <copyright file="UsedCardManager.cs" company="Delft University of Technology">
 //     Copyright 2015, Delft University of Technology
-//     
+//
 //     This software is licensed under the terms of the MIT License.
-//     A copy of the license should be included with this software. If not, 
+//     A copy of the license should be included with this software. If not,
 //     see http://opensource.org/licenses/MIT for the full license.
 // </copyright>
 //----------------------------------------------------------------------------
@@ -11,8 +11,14 @@ namespace Projection
 {
     using UnityEngine;
 
+    /// <summary>
+    /// Indicates the BaseForLevel instance used for synchronizing positions.
+    /// </summary>
     public class UsedCardManager : MonoBehaviour
     {
+        /// <summary>
+        /// Gets or sets the currently used BaseForLevel instance.
+        /// </summary>
         public BaseForLevel CurrentlyUsed { get; set; }
     }
 }
