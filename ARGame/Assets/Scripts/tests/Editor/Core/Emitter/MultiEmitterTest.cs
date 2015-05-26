cf1500588c04931f8ea82095c7d323f989fbae31
thijs boumans <thijsboumans@online.nl>
2015-05-26 15:54:50 +0200

    moved tests over

---
+++ ARGame/Assets/Scripts/tests/Editor/Core/Emitter/MultiEmitterTest.cs
@@ -0,0 +1,70 @@
+﻿//----------------------------------------------------------------------------
+// <copyright file="MultiEmitterTest.cs" company="Delft University of Technology">
+//     Copyright 2015, Delft University of Technology
+//     
+//     This software is licensed under the terms of the MIT License.
+//     A copy of the license should be included with this software. If not, 
+//     see http://opensource.org/licenses/MIT for the full license.
+// </copyright>
+//----------------------------------------------------------------------------
+namespace Core.Emitter
+{
+    using System;
+    using NSubstitute;
+    using NUnit.Framework;
+    using TestUtilities;
+    using UnityEngine;
+
+    /// <summary>
+    /// A test class for the multi emitter.
+    /// </summary>
+    [TestFixture]
+    public class MultiEmitterTest : MirrorsUnitTest
+    {
+        /// <summary>
+        /// Tests if the correct exception is thrown when 
+        /// ApplyProperties is called with a null reference.
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ApplyTest()
+        {
+            MultiEmitter.ApplyProperties(null, GameObjectFactory.CreateTestBeam());
+        }
+
+        /// <summary>
+        /// Tests if the correct exception is thrown when 
+        /// ApplyProperties is called with a null reference.
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ApplyTest2()
+        {
+            MultiEmitter.ApplyProperties(CreateEmitter().GetComponent<LineRenderer>(), null);
+        }
+
+        /// <summary>
+        /// Tests if the result of a valid call
+        /// to ApplyProperties is not null.
+        /// </summary>
+        [Test]
+        public void ApplyTestValid()
+        {
+            Assert.NotNull(MultiEmitter.ApplyProperties(CreateEmitter().GetComponent<LineRenderer>(), GameObjectFactory.CreateTestBeam()));
+        }
+
+        /// <summary>
+        /// Tests if the enabling and disabling
+        /// of lasers functions as it should.
+        /// </summary>
+        [Test]
+        public void EnableDisableTest()
+        {
+            MultiEmitter me = CreateMultiEmitter();
+            LaserEmitter le = me.GetEmitter(CreateTestBeam());
+            Assert.True(le.Enabled);
+            me.DisableAll();
+            Assert.False(le.Enabled);
+        }
+    }
+}
---
+++ ARGame/Assets/Scripts/tests/Editor/Core/Receiver/OrGateTest.cs
@@ -0,0 +1,86 @@
+﻿//----------------------------------------------------------------------------
+// <copyright file="OrGateTest.cs" company="Delft University of Technology">
+//     Copyright 2015, Delft University of Technology
+//     
+//     This software is licensed under the terms of the MIT License.
+//     A copy of the license should be included with this software. If not, 
+//     see http://opensource.org/licenses/MIT for the full license.
+// </copyright>
+//----------------------------------------------------------------------------
+namespace Core.Receiver
+{
+    using System;
+    using Core.Emitter;
+    using NUnit.Framework;
+    using TestUtilities;
+    using UnityEngine;
+
+    /// <summary>
+    /// A test class for the OrGate object.
+    /// </summary>
+    [TestFixture]
+    public class OrGateTest : MirrorsUnitTest
+    {
+        /// <summary>
+        /// Tests if a MultiEmitter object is added when 
+        /// the object is instantiated in Unity.
+        /// </summary>
+        [Test]
+        public void StartTest()
+        {
+            OrGate og = CreateOrGate();
+            Assert.True(og.PassThroughEmitter == null);
+            og.Start();
+            Assert.False(og.PassThroughEmitter == null);
+        }
+
+        /// <summary>
+        /// Tests if the other variables 
+        /// are correctly instantiated.
+        /// </summary>
+        [Test]
+        public void StartTest2()
+        {
+            OrGate og = CreateOrGate();
+            og.Start();
+            Assert.False(og.BeamCreated);
+        }
+
+        /// <summary>
+        /// Tests if the correct exception is thrown when
+        /// an invalid HitEventArgs is used in OnLaserHit.
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidOnLaserHit()
+        {
+            OrGate og = CreateOrGate();
+            og.OnLaserHit(null, new HitEventArgs());
+        }
+
+        /// <summary>
+        /// Tests if the correct exception is thrown when
+        /// a null reference HitEventArgs is used in OnLaserHit.
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullOnLaserHit()
+        {
+            OrGate og = CreateOrGate();
+            og.OnLaserHit(null, null);
+        }
+
+        /// <summary>
+        /// Tests if a beam is created, when one laser hits
+        /// the gate.
+        /// </summary>
+        [Test]
+        public void OneLaserHit()
+        {
+            OrGate og = CreateOrGate();
+            og.Start();
+            og.OnLaserHit(null, MirrorsUnitTest.CreateTestHit());
+            Assert.True(og.BeamCreated);
+        }
+    }
+}
