f98498fc6846fea200c1f6bf358069292dfdf27a
thijs boumans <thijsboumans@online.nl>
2015-05-28 13:45:11 +0200

    with wrapper?:

---
+++ ARGame/Assets/Scripts/Projection/UpdateWrapper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using Network;
+namespace Projection{
+public class UpdateWrapper : MonoBehaviour {
+		/// <summary>
+		/// wraps a position update to allow assignment to gameObjects
+		/// </summary>
+		public PositionUpdate wrapped;
+
+	}
+
+}
