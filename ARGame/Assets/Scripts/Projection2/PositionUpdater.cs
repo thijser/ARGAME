3c79f77665e73eccf6a3da166cb3e119a7ddec16
Tim van Rossum <trvanrossum@gmail.com>
2015-06-02 10:23:58 +0200

    StyleCopping now.

--- ARGame/Assets/Scripts/Projection2/PositionUpdater.cs
+++ ARGame/Assets/Scripts/Projection2/PositionUpdater.cs
@@ -1,76 +1,113 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-using Network;
+﻿//----------------------------------------------------------------------------
+// <copyright file="PositionUpdater.cs" company="Delft University of Technology">
+//     Copyright 2015, Delft University of Technology
+//
+//     This software is licensed under the terms of the MIT License.
+//     A copy of the license should be included with this software. If not,
+//     see http://opensource.org/licenses/MIT for the full license.
+// </copyright>
+//----------------------------------------------------------------------------
+namespace Projection
+{
+    using System.Collections.Generic;
+    using Network;
+    using UnityEngine;
 
-namespace Projection{
-	public class PositionUpdater : MonoBehaviour {
-		/// <summary>
-		/// How long are we willing to wait after losing track of a marker. 
-		/// </summary>
-		public long patience=(1000*10000);//1000 milliseconds 
+    /// <summary>
+    /// A class that handles marker registration and updates positions.
+    /// </summary>
+    public class PositionUpdater : MonoBehaviour 
+    {
+        /// <summary>
+        /// How long are we willing to wait after losing track of a marker. 
+        /// </summary>
+        public long patience=(1000*10000);//1000 milliseconds 
 
-		/// <summary>
-		/// Central level marker, this should be visible. 
-		/// </summary>
-		Marker parent;
-		public float scale=1; 
-		/// <summary>
-		/// Collection of all registered to this clas. 
-		/// </summary>
-		private Dictionary<int, Marker> markerTable;
-		/// <summary>
-		/// register a new marker
-		/// </summary>
-		public void OnMarkerRegister(MarkerRegister reg){
-			markerTable.Add(reg.getMarker().id,reg.getMarker());
-			if(parent==null)
-				parent=reg.getMarker();
-		}
-		/// <summary>
-		/// gets marker with Identifier  
-		/// </summary>
-		/// <returns>The marker.</returns>
-		/// <param name="id">Identifier.</param>
-		/// <exception cref="KeyNotFoundException"> thrown when the marker is not (yet) registered</exception>
-		public Marker GetMarker(int id){
-			if(markerTable.ContainsKey(id)){
-				return markerTable[id];
-			}else{
-				throw new KeyNotFoundException("this marker is not registered");
-			}
-		}
-		/// <summary>
-		/// this marker has been seen by remote, informs the marker of this 
-		/// </summary>
-		/// <param name="mp">marker position.</param>
-		/// <param name="id">Identifier.</param>
-		public void OnMarkerSeen(MarkerPosition mp,int id){
-			this.GetMarker(id).SetLocalPosition(mp);
-			if(parent.localPosition.timeStamp.Ticks+patience<mp.timeStamp.Ticks){
-				reparent(this.GetMarker(id));
-			}
-		}
-		/// <summary>
-		/// inform marker that it has received an rotationUpdate 
-		/// </summary>
-		/// <param name="ru">rotation update.</param>
-		public void onRotationUpdate(RotationUpdate ru){
-			this.GetMarker(ru.ID).SetObjectRotation(ru.Rotation);
-		}
-		/// <summary>
-		/// Update position of all markers .
-		/// </summary>
-		public void Update(){
-			foreach(KeyValuePair<int, Marker> entry in markerTable)
-			{
-				updatePosition(entry.Value);
-			}
-		}
-		/// <summary>
-		/// uses the market target and parent to set the transform of target
-		/// </summary>
-		/// <param name="target">Target</param>
-		public void updatePosition(Marker target)
+        /// <summary>
+        /// Central level marker, this should be visible. 
+        /// </summary>
+        Marker parent;
+
+        /// <summary>
+        /// Scale of the object.
+        /// </summary>
+        public float scale = 1;
+ 
+        /// <summary>
+        /// Collection of all registered to this class. 
+        /// </summary>
+        private Dictionary<int, Marker> markerTable;
+
+        /// <summary>
+        /// Register a new marker
+        /// <param name="register">The marker register parameter that registers the new marker.</param>
+        /// </summary>
+        public void OnMarkerRegister(MarkerRegister register)
+        {
+            markerTable.Add(register.getMarker().id, register.getMarker());
+            if (parent == null)
+            {
+                parent = register.getMarker();
+            }  
+        }
+
+        /// <summary>
+        /// Gets marker with Identifier  
+        /// </summary>
+        /// <returns>The marker.</returns>
+        /// <param name="id">The identifier.</param>
+        /// <exception cref="KeyNotFoundException"> thrown when the marker is not (yet) registered</exception>
+        public Marker GetMarker(int id)
+        {
+            if(markerTable.ContainsKey(id))
+            {
+                return markerTable[id];
+            }
+            else
+            {
+                throw new KeyNotFoundException("this marker is not registered");
+            }
+        }
+
+        /// <summary>
+        /// This marker has been seen by remote, informs the marker of this 
+        /// </summary>
+        /// <param name="mp">marker position.</param>
+        /// <param name="id">Identifier.</param>
+        public void OnMarkerSeen(MarkerPosition mp,int id)
+        {
+            this.GetMarker(id).SetLocalPosition(mp);
+            if(parent.localPosition.timeStamp.Ticks+patience<mp.timeStamp.Ticks)
+            {
+                reparent(this.GetMarker(id));
+            }
+        }
+
+        /// <summary>
+        /// inform marker that it has received an rotationUpdate 
+        /// </summary>
+        /// <param name="ru">rotation update.</param>
+        public void onRotationUpdate(RotationUpdate ru)
+        {
+            this.GetMarker(ru.ID).SetObjectRotation(ru.Rotation);
+        }
+
+        /// <summary>
+        /// Update position of all markers .
+        /// </summary>
+        public void Update()
+        {
+            foreach(KeyValuePair<int, Marker> entry in markerTable)
+            {
+                updatePosition(entry.Value);
+            }
+        }
+
+        /// <summary>
+        /// uses the market target and parent to set the transform of target
+        /// </summary>
+        /// <param name="target">Target</param>
+        public void updatePosition(Marker target)
         {
             if (target == parent)
             {
@@ -80,7 +117,7 @@ namespace Projection{
             {
                 UpdateChildPosition(target);
             }
-		}
+        }
 
         /// <summary>
         /// Updates position if supplied target is the parent.
@@ -104,23 +141,28 @@ namespace Projection{
             target.gameObject.transform.position = target.remotePosition.Position - parent.remotePosition.Position;
             // TODO: If mirrored then swap operation params.
         }
-		public void reparent(Marker target){
-			parent=target;
-			foreach(KeyValuePair<int, Marker> entry in markerTable){
-				if(entry.Value!=parent){
-					entry.Value.transform.SetParent(target.transform);
-					updatePosition(entry.Value);
-				}
-			}
-		}
 
-		/// <summary>
-		/// set the location of the marker based on the remote position. 
-		/// </summary>
-		/// <param name="pu">position update received over the net.</param>
-		public void OnPositionUpdate (PositionUpdate pu){
-			this.GetMarker(pu.ID).SetRemotePosition(new MarkerPosition(pu));
-		}
-	}
 
+        public void reparent(Marker target)
+        {
+            parent=target;
+            foreach(KeyValuePair<int, Marker> entry in markerTable)
+            {
+                if(entry.Value!=parent)
+                {
+                    entry.Value.transform.SetParent(target.transform);
+                    updatePosition(entry.Value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// set the location of the marker based on the remote position. 
+        /// </summary>
+        /// <param name="pu">position update received over the net.</param>
+        public void OnPositionUpdate (PositionUpdate pu)
+        {
+            this.GetMarker(pu.ID).SetRemotePosition(new MarkerPosition(pu));
+        }
+    }
 }
