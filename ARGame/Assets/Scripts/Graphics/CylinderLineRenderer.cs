f8cab3353a2f76c933d68254aa9fd502b7b237da
Alexander Overvoorde <overv161@gmail.com>
2015-06-08 14:25:21 +0200

    Add CylinderLineRenderer to Graphics namespace

--- ARGame/Assets/Scripts/Graphics/CylinderLineRenderer.cs
+++ ARGame/Assets/Scripts/Graphics/CylinderLineRenderer.cs
@@ -2,64 +2,73 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class CylinderLineRenderer : MonoBehaviour
+namespace Graphics
 {
-    public Vector3[] Positions;
-    public Material LineMaterial;
-    public float LineWidth = 1.0f;
-    public bool UseWorldSpace = true;
-
-    private MeshFilter meshFilter;
-    private MeshRenderer meshRenderer;
-
-    public void Start()
+    /// <summary>
+    /// Custom LineRenderer for lasers that doesn't have the rendering problems
+    /// that occur in the Unity LineRenderer implementation.
+    /// 
+    /// NOTE: Implementation assumes that up is always [0, 1, 0].
+    /// </summary>
+    public class CylinderLineRenderer : MonoBehaviour
     {
-        // Create components for rendering line mesh
-        meshFilter = gameObject.AddComponent<MeshFilter>();
-        meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        public Vector3[] Positions;
+        public Material LineMaterial;
+        public float LineWidth = 1.0f;
 
-        meshFilter.mesh = new Mesh();
-    }
+        private MeshFilter meshFilter;
+        private MeshRenderer meshRenderer;
 
-    public void Update()
-    {
-        // Update line material
-        meshRenderer.material = LineMaterial;
+        public void Start()
+        {
+            // Create components for rendering line mesh
+            meshFilter = gameObject.AddComponent<MeshFilter>();
+            meshRenderer = gameObject.AddComponent<MeshRenderer>();
 
-        // Create mesh that represents line
-        Mesh mesh = meshFilter.mesh;
-
-        List<Vector3> vertices = new List<Vector3>();
-        List<int> triangles = new List<int>();
-
-        for (int i = 0; i < Positions.Length - 1; i++) {
-            AddLineSegment(Positions[i], Positions[i + 1], vertices, triangles);
+            meshFilter.mesh = new Mesh();
         }
 
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
+        public void Update()
+        {
+            // Update line material
+            meshRenderer.material = LineMaterial;
 
-        mesh.RecalculateNormals();
-    }
+            // Create mesh that represents line
+            Mesh mesh = meshFilter.mesh;
 
-    private void AddLineSegment(Vector3 from, Vector3 to, List<Vector3> vertices, List<int> triangles)
-    {
-        int offset = vertices.Count;
+            List<Vector3> vertices = new List<Vector3>();
+            List<int> triangles = new List<int>();
 
-        triangles.Add(offset + 0);
-        triangles.Add(offset + 1);
-        triangles.Add(offset + 2);
+            for (int i = 0; i < Positions.Length - 1; i++)
+            {
+                AddLineSegment(Positions[i], Positions[i + 1], vertices, triangles);
+            }
 
-        triangles.Add(offset + 2);
-        triangles.Add(offset + 3);
-        triangles.Add(offset + 0);
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
 
-        Vector3 left = Vector3.Cross(from - to, Vector3.up).normalized;
-        Vector3 up = Vector3.up;
+            mesh.RecalculateNormals();
+        }
 
-        vertices.Add(from - left * LineWidth / 2);
-        vertices.Add(to - left * LineWidth / 2);
-        vertices.Add(to + left * LineWidth / 2);
-        vertices.Add(from + left * LineWidth / 2);
+        private void AddLineSegment(Vector3 from, Vector3 to, List<Vector3> vertices, List<int> triangles)
+        {
+            int offset = vertices.Count;
+
+            triangles.Add(offset + 0);
+            triangles.Add(offset + 1);
+            triangles.Add(offset + 2);
+
+            triangles.Add(offset + 2);
+            triangles.Add(offset + 3);
+            triangles.Add(offset + 0);
+
+            Vector3 left = Vector3.Cross(from - to, Vector3.up).normalized;
+            Vector3 up = Vector3.up;
+
+            vertices.Add(from - left * LineWidth / 2);
+            vertices.Add(to - left * LineWidth / 2);
+            vertices.Add(to + left * LineWidth / 2);
+            vertices.Add(from + left * LineWidth / 2);
+        }
     }
 }
