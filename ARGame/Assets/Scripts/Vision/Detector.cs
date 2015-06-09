0bcc0d565d2e0e490902c0aeead0a205157b2586
thijs boumans <thijsboumans@online.nl>
2015-06-09 12:45:56 +0200

    finished merge

--- ARGame/Assets/Scripts/Vision/Detector.cs
+++ ARGame/Assets/Scripts/Vision/Detector.cs
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-//----------------------------------------------------------------------------
-// <copyright file="Detector.cs" company="Delft University of Technology">
-//     Copyright 2015, Delft University of Technology
-//
-//     This software is licensed under the terms of the MIT License.
-//     A copy of the license should be included with this software. If not,
-//     see http://opensource.org/licenses/MIT for the full license.
-// </copyright>
-//----------------------------------------------------------------------------
-namespace Vision
-{
-    using System.Collections.ObjectModel;
-    using Projection;
-    using UnityEngine;
-
-    /// <summary>
-    /// Broadcasts the MarkerPositions from an <see cref="IARLink"/> instance
-    /// as Unity messages.
-    /// </summary>
-    public class Detector : MonoBehaviour
-    {
-        /// <summary>
-        /// Gets or sets the IARLink instance used to get MarkerPositions from.
-        /// </summary>
-        public IARLink Link { get; set; }
-
-        /// <summary>
-        /// Initializes the Detector with a MetaLink instance.
-        /// </summary>
-        public void Start()
-        {
-			Link= gameObject.GetComponent<MetaListLink>();
-			//if(Link==null)
-			//	this.Link = new GameObject("MetaLink", typeof(MetaListLink)).GetComponent<MetaListLink>();
-        }
-
-        /// <summary>
-        /// Retrieves the MarkerPositions from the <see cref="IARLink"/> 
-        /// instance and broadcasts all positions as "OnMarkerSeen" Unity 
-        /// messages.
-        /// </summary>
-        public void LateUpdate()
-        {
-            Collection<MarkerPosition> list = this.Link.GetMarkerPositions();
-            foreach (MarkerPosition mp in list)
-            {
-                Debug.Log("seen: " +mp.ToString());
-                this.SendMessage(
-                    "OnMarkerSeen",
-                    mp);
-            }
-        }
-    }
-=======
 //----------------------------------------------------------------------------
 // <copyright file="Detector.cs" company="Delft University of Technology">
 //     Copyright 2015, Delft University of Technology
@@ -100,10 +45,8 @@ namespace Vision
                 Debug.Log(mp.ToString());
                 this.SendMessage(
                     "OnMarkerSeen",
-                    mp,
-                    SendMessageOptions.DontRequireReceiver);
+                    mp);
             }
         }
     }
->>>>>>> e8af26904501702af706160b745bb17239b01a13
 }
