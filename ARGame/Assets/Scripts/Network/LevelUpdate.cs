47ef31f0f9ed4f23f2415e6fc5cc503b1256eb59
Tim van Rossum <trvanrossum@gmail.com>
2015-06-12 14:07:10 +0200

    Changed BoardSizeUpdate to LevelUpdate.

--- ARGame/Assets/Scripts/Network/LevelUpdate.cs
+++ ARGame/Assets/Scripts/Network/LevelUpdate.cs
@@ -9,28 +9,41 @@
     /// Usually, this message is sent only once just after the client connects.
     /// </para>
     /// </summary>
-    public class BoardSizeUpdate : AbstractUpdate
+    public class LevelUpdate : AbstractUpdate
     {
         /// <summary>
         /// Initializes a new object of the <see cref="BoardSizeUpdate"/> class.
         /// </summary>
         /// <param name="size">The new size of the board.</param>
+        /// <param name="index">The index of the next level.</param>
         /// <exception cref="ArgumentOutOfRangeException">
-        /// If one of the values in the supplied <see cref="Vector2"/> is not positive
+        /// If one of the values in the supplied <see cref="Vector2"/> is not positive,
+        /// or if the supplied level index is not positive.
         /// </exception>
-        public BoardSizeUpdate(Vector2 size)
+        public LevelUpdate(Vector2 size, int index)
         {
             if (size.x <= 0 || size.y <= 0)
             {
                 throw new ArgumentOutOfRangeException("size", size, "The board must have positive dimensions");
             }
 
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("index", index, "The index of the next level must be a positive integer.");
+            }
+
             this.Size = size;
+            this.NextLevelIndex = index;
         }
 
         /// <summary>
         /// Gets the new board size.
         /// </summary>
         public Vector2 Size { get; private set; }
+
+        /// <summary>
+        /// Gets the index of the next level.
+        /// </summary>
+        public int NextLevelIndex { get; private set; }
     }
 }
