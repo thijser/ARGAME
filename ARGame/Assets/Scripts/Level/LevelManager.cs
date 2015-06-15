9b8231839040ec951fb62551fb527da0c07c00fa
thijs boumans <thijsboumans@online.nl>
2015-06-15 13:02:45 +0200

    working levelmanager

--- ARGame/Assets/Scripts/Level/LevelManager.cs
+++ ARGame/Assets/Scripts/Level/LevelManager.cs
@@ -2,13 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 namespace Level{
-public class LevelManager : MonoBehaviour {
-	public LevelLoader levelLoader= new LevelLoader();
-	// Use this for initialization
-	void Start () {
-			levelLoader.CreateLevel("C:/Users/thijs/Desktop/ARGAME/Levels/example.txt");
+	public class LevelManager : MonoBehaviour {
+		private LevelLoader levelLoader= new LevelLoader();
+		private GameObject level;
+		int currentLevelIndex=0;
+
+		void Start() {
+				loadLevel(0);
+		}
+		public void nextLevel(){
+			loadLevel(currentLevelIndex);
+		}
+		public void restartLevel(){
+			loadLevel(currentLevelIndex);	
+		}
+		public void restartGame(){
+			loadLevel (0);
+		}
+
+		public void loadLevel(int index){
+				Destroy(level);
+				Debug.Log ("loading level"+index);
+				level=levelLoader.CreateLevel("Assets/resources/Levels/"+index+".txt");
+		}
 	}
-
-}
-
 }
