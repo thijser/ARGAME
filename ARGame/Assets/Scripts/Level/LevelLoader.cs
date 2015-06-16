0240947500e4b3911ca84839ece63c36b450c8af
thijs boumans <thijsboumans@online.nl>
2015-06-16 14:17:46 +0200

    fremote not local position

--- ARGame/Assets/Scripts/Level/LevelLoader.cs
+++ ARGame/Assets/Scripts/Level/LevelLoader.cs
@@ -96,7 +96,7 @@
 			level.AddComponent<Marker>();
 			Marker m = level.GetComponent<Marker>();
 			m.ID=13379001;
-			m.LocalPosition=new MarkerPosition(Vector3.zero,Quaternion.identity,DateTime.Now,Vector3.zero,13379001);
+			m.RemotePosition=new MarkerPosition(Vector3.zero,Quaternion.identity,DateTime.Now,Vector3.zero,13379001);
 			return level;
         }
     }
