af049013d238b2ef410fd488fcbc8cfe5b51b4ec
thijs boumans <thijsboumans@online.nl>
2015-06-19 15:33:14 +0200

    with a head

---
+++ ARGame/Assets/Scripts/Camera/FollowPlayerCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayerCamera : MonoBehaviour {
+	public int id;
+	public void OnFollowPlayerInfo(FollowPlayerInfo playerInfo){
+		if(playerInfo.playerID==id){
+			transform.position=playerInfo.position;
+			transform.rotation=Quaternion.Euler(playerInfo.rotation);
+		}
+	}
+}
---
+++ ARGame/Assets/Scripts/Camera/FollowPlayerInfo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Projection;
+public class FollowPlayerInfo {
+
+	public Vector3 position{get;set;}
+	public Vector3 rotation{get;set;}
+	public int playerID{get;set;}
+	private Transform lamb;
+ 	void Start(){
+		GameObject l= new GameObject("cattle");
+		lamb=l.transform;
+	}
+	public FollowPlayerInfo(int id,MarkerHolder mh){
+
+		Marker parent=mh.Parent;
+		Matrix4x4 posmatrix=parent.TransformMatrix.inverse;
+		lamb.SetFromMatrix(posmatrix);
+		position=lamb.position;
+		rotation=lamb.rotation.eulerAngles;
+		playerID=id;
+	}
+}
