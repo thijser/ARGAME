771606ca137695dd3dc537a3c196d54373e2fed2
thijs boumans <thijsboumans@online.nl>
2015-06-19 12:47:08 +0200

    added rotcamerarig

---
+++ ARGame/Assets/Scripts/Camera/RotCam.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotCam : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		onArrowRotate();
+	}
+
+	void onArrowRotate(){
+		if(Input.GetKey(KeyCode.RightArrow)){
+			transform.Rotate(transform.forward*-1*speed*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.Rotate(transform.forward*speed*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.UpArrow)){
+			transform.Rotate(transform.right*speed*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.DownArrow)){
+				transform.Rotate(-1*transform.right*speed*Time.deltaTime);
+		}
+	}
+}
