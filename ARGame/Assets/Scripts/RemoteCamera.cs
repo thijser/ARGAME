44f0c9c2b0b4dc654c38c1a8088e2929837383f8
Tim van Rossum <trvanrossum@gmail.com>
2015-06-22 11:33:19 +0200

    Rescaled more levels + fixed an error in the RemoteCamera script.

--- ARGame/Assets/Scripts/RemoteCamera.cs
+++ ARGame/Assets/Scripts/RemoteCamera.cs
@@ -133,7 +133,7 @@ public class RemoteCamera : MonoBehaviour
     public void UpdateCamera()
     {
         Vector3 position = new Vector3(0, 0, -this.Distance);
-        this.GetComponentInChildren<Camera>().transform.position = position + this.Target.position;
+        this.GetComponentInChildren<Camera>().transform.position = position + this.Target.transform.position;
         this.transform.localRotation = Quaternion.Euler(this.Altitude, this.Angle, 0);
     }
 }
