842e87198485f7338268c701ec3508ee5c2ca3fd
Patrick Kramer <ptrck.krmr@gmail.com>
2015-06-29 09:36:31 +0200

    Improved LocalPlayerSimulator.
    This now also simulates a board size, fixing the offset of the simulated
    local player.

--- ARGame/Assets/Scripts/Camera/LocalPlayerSimulator.cs
+++ ARGame/Assets/Scripts/Camera/LocalPlayerSimulator.cs
@@ -1,5 +1,7 @@
 ﻿namespace Camera
 {
+    using Core;
+    using Level;
     using Network;
     using Projection;
     using UnityEngine;
@@ -11,6 +13,11 @@
     public class LocalPlayerSimulator : MonoBehaviour
     {
         /// <summary>
+        /// The size of the simulated board.
+        /// </summary>
+        public Vector2 SimulatedBoardSize = new Vector2(8f, 10f);
+
+        /// <summary>
         /// The simulated player Id.
         /// </summary>
         public int PlayerId = 1001;
@@ -21,13 +28,19 @@
         private RemotePlayerMarker marker;
 
         /// <summary>
-        /// Initializes a RemotePlayerMarker to use.
+        /// Initializes the LocalPlayerSimulator and simulates a board.
         /// </summary>
         public void Start()
         {
             HeadHolder holder = this.transform.parent.GetComponent<HeadHolder>();
             this.marker = holder.GetPlayer(this.PlayerId);
             holder.Follow(this.marker);
+            
+            this.transform.parent.GetComponent<BoardResizer>().UpdateBoardSize(this.SimulatedBoardSize);
+            
+            LevelManager manager = this.transform.parent.GetComponent<LevelManager>();
+            manager.BoardSize = this.SimulatedBoardSize;
+            manager.RestartLevel();
         }
 
         /// <summary>
