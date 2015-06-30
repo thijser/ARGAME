3ea93f528739e95947829f7bbc8c878080bb4909
Patrick Kramer <ptrck.krmr@gmail.com>
2015-06-30 11:54:50 +0200

    Fixed issues caused by merge.

--- ARGame/Assets/Scripts/Projection/RemoteMarker.cs
+++ ARGame/Assets/Scripts/Projection/RemoteMarker.cs
@@ -45,10 +45,10 @@ namespace Projection
         {
             if (this.RemotePosition != null)
             {
-                this.RemotePosition.Scale = this.ScaleFactor * Vector3.one;
+                this.RemotePosition.Scale = this.ScaleFactor * new Vector3(1, 1, 1);
             }
             
-            base.UpdatePosition(transformMatrix);
+            base.UpdatePosition(Matrix4x4.Scale(new Vector3(1, 1, -1)) * transformMatrix);
 		}
     }
 }
