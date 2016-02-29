9e70972fd16337a209f6d36071ffe3d0694c9237
Alexander Overvoorde <overv161@gmail.com>
2016-02-29 10:06:04 +0100

    Add basic win screen

---
+++ ARGame/Assets/Scripts/Core/WinScreen.cs
@@ -0,0 +1,33 @@
+﻿namespace Core {
+    using UnityEngine;
+
+    public class WinScreen : MonoBehaviour {
+        public readonly float TransitionTime = 5;
+
+        private TextMesh MainText;
+        private TextMesh SubText;
+        private Camera OrthoCamera;
+
+        private bool ShowingWinScreen;
+        private float ;
+        private int 
+
+        public void Start() {
+            MainText = transform.Find("MainText").GetComponent<TextMesh>();
+            SubText = transform.Find("SubText").GetComponent<TextMesh>();
+
+            OrthoCamera = transform.Find("Camera").GetComponent<Camera>();
+
+            // Initially don't draw the win screen
+            OrthoCamera.enabled = false;
+        }
+
+        public void FinishLevel(int timeSpent, int nextLevel) {
+
+        }
+
+        public void Update() {
+            
+        }
+    }
+}
